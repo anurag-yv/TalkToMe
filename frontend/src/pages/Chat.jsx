4dedf7dc5a621164c13764d61342fc1a69b8c067
Anurag <anuragyadavmzp2006@gmail.com>
2025-08-11 22:30:10 +0530

    update

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,7 +1,7 @@
 // At the top or above your export
 function Chat(props) {
   // your chat code here...
-  return <div>Chat works!</div>;
+  return <div>Chat works and it will!</div>;
 }
 
 // At the bottom
